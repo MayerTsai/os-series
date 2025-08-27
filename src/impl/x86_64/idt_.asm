a8703d36336ec7f70c9942edfb061f9bb4107e2b
MayerTsai <mayer.tsai@gmail.com>
2025-08-27 19:58:20 +0800

    clone from David Callanan

--- src/impl/x86_64/idt_.asm
+++ src/impl/x86_64/idt_.asm
@@ -5,45 +5,53 @@ idt_load:
 	lidt [rdi]
 	ret
 
+%macro PUSH_ALL_GPRS 0
+	; save general-purpose registers
+	push rax
+	push rbx
+	push rcx
+	push rdx
+	push rbp
+	push rsi
+	push rdi
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+%endmacro
+
+%macro POP_ALL_GPRS 0
+	; restore general-purpose registers
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop rdi
+	pop rsi
+	pop rbp
+	pop rdx
+	pop rcx
+	pop rbx
+	pop rax
+%endmacro
+
 %macro WRAPPED_HANDLER 1
 	global %1_wrapped
 	
 	%1_wrapped:
-		; save general-purpose registers
-		push rax
-		push rbx
-		push rcx
-		push rdx
-		push rbp
-		push rsi
-		push rdi
-		push r8
-		push r9
-		push r10
-		push r11
-		push r12
-		push r13
-		push r14
-		push r15
+		PUSH_ALL_GPRS
 
 		call %1
 
-		; restore general-purpose registers
-		pop r15
-		pop r14
-		pop r13
-		pop r12
-		pop r11
-		pop r10
-		pop r9
-		pop r8
-		pop rdi
-		pop rsi
-		pop rbp
-		pop rdx
-		pop rcx
-		pop rbx
-		pop rax
+		POP_ALL_GPRS
 		
 		iretq
 %endmacro
